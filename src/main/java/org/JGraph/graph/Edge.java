17a76404bedf26126295aaa7fed69e11fcaa9a49
BynBang <226378661+BynBangsAlts@users.noreply.github.com>
2026-01-30 14:19:30 +0100

    Refactor edge management and add reverse edge method
    Refactor addEdge method to improve edge handling and add addReverseEdge method.

--- src/main/java/org/JGraph/graph/Edge.java
+++ src/main/java/org/JGraph/graph/Edge.java
@@ -2,11 +2,12 @@ package org.JGraph.graph;
 
 import org.JGraph.JGraph;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Edge {
 
+    public static Object[] string;
+    public static List<Edge> edges;
     /**
      * {from=[to]}
      *
@@ -14,13 +15,26 @@ public class Edge {
      * @param to   EndPoint
      */
     public static void addEdge(String from, String to) {
-        List<String> edges = JGraph.getMap().computeIfAbsent(from, k -> new ArrayList<>());
-        if (String.valueOf(JGraph.getYes()).equals("true")) {
-            if (!edges.contains(to)) {
-                edges.add(to);
-            }
+        JGraph.getMap().putIfAbsent(from, new ArrayList<>());
+        JGraph.getMap().putIfAbsent(to, new ArrayList<>());
+
+        List<String> edges = JGraph.getMap().get(from);
+        if (JGraph.getYes()) {
+            edges.add(to);
+            return;
+        }
+
+        if (!edges.contains(to)) {
+            edges.add(to);
         }
     }
+
+    /**
+     * LOL
+     * @param to reverse start
+     * @param from reverse end
+     */
+    public static void addReverseEdge(String to, String from) {
+        addEdge(from, to);
+    }
 }
-
-
